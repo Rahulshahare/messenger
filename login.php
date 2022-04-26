52e5a9af1e050e9ae4d898f5726ddc5bede4552a
rahulshahare <rahulshahare@gmail.com>
2022-04-26 22:20:44 +0530

    login form added

--- login.php
+++ login.php
@@ -1,3 +1,8 @@
+<?php
+    if(!empty($_POST)){
+        print_r($_POST);
+    }
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -20,8 +25,22 @@
     </nav>
     <div class="container">
         <div class="row justify-content-md-center">
-            <div class="col-md-4 mt-3 p-2 mybox-shadow">
-                <h3 class="text-center">Login</h3>
+            <div class="col-md-4 mt-5 p-3 mybox-shadow">
+                <h3 class="text-center mb-3">Login</h3>
+                <form action="login.php" method="POST">
+                    <div class="mb-3">
+                        <label for="Email" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="Email" name="email" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="Password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="Password" required name="password">
+                    </div>
+                    <div class="mb-3 d-grid">
+                    <button type="submit" class="btn btn-primary btn-block">Login</button>
+                    </div>
+                </form>
+                <p>New to site, <a href="createnewuser.php">Create a account</a></p>
             </div>
         </div>
     </div>

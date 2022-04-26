9e8c2c94990c29344161d098b7685a4b40d1bdb0
rahulshahare <rahulshahare@gmail.com>
2022-04-26 22:46:05 +0530

    checking errors first like empty inputs & invalid email & already exist email

--- createnewuser.php
+++ createnewuser.php
@@ -1,6 +1,33 @@
 <?php
+$error = '';
     if(!empty($_POST)){
         print_r($_POST);
+        $fullname = $_POST["fullname"];
+        $email = $_POST["fullname"];
+        $password = $_POST["fullname"];
+
+        if(empty(trim($fullname)) || empty(trim($email)) || empty(trim($password))){
+            $error = "Name, Email or Password are empty.";
+        }
+
+        if(empty($error) && !empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL)===false){
+            $error = 'Invalid email, Please enter valid email.';
+        }
+
+        if(empty($error)){
+            //email is unique in system
+            include_once"database/db.php";
+            $stm = $dbh->prepare('SELECT * FROM user WHERE email = :email');
+            $stm->bindValue(':email',$email);
+            $stm->execute();
+            $count = $stm->rowCount();
+            if($count > 0){
+                $error = "Email already exist,Try with new email.";
+            }else{
+
+            }
+
+        }
     }
 ?>
 <!doctype html>
@@ -12,7 +39,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/messenger.css" rel="stylesheet">
+    <link href="css/messenger.css?v=0.01" rel="stylesheet">
 
     <title>Messenger</title>
   </head>
@@ -26,14 +53,19 @@
     <div class="container">
         <div class="row justify-content-md-center">
             <div class="col-md-4 mt-5 p-3 mybox-shadow">
-                <h3 class="text-center mb-3">Create new account</h3>
+                <h5 class="text-center mb-3 font_weight300">Create new account</h5>
+                <?php if($error != ''){?>
+                    <div class="alert alert-danger" role="alert">
+                       <?php echo $error; ?>
+                    </div>
+                <?php }?>
                 <form action="createnewuser.php" method="POST">
-                <div class="mb-3">
+                    <div class="mb-3">
                         <label for="Fullname" class="form-label">Full Name</label>
                         <input type="text" class="form-control" id="Fullname" name="fullname" required>
                     </div>
                     <div class="mb-3">
-                        <label for="Email" class="form-label">Email address</label>
+                        <label for="Email" class="form-label">Email</label>
                         <input type="email" class="form-control" id="Email" name="email" required>
                     </div>
                     <div class="mb-3">

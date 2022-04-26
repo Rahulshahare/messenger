a6260c10c79f01f17c7b1d540655dd2d60189110
rahulshahare <rahulshahare@gmail.com>
2022-04-26 20:55:38 +0530

    a page for login

---
+++ login.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/messenger.css" rel="stylesheet">
+
+    <title>Messenger</title>
+  </head>
+  <body>
+   
+    <nav class="navbar navbar-dark bg-primary">
+        <div class="container">
+            <span class="navbar-brand mb-0 h1">Messenger</span>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-md-4 mt-3 p-2 mybox-shadow">
+                <h3 class="text-center">Login</h3>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/messenger.js"></script>
+  </body>
+</html>

60ed16845069d01c5896cfc5e6f3a643987b5159
rahulshahare <rahulshahare@gmail.com>
2022-04-26 22:21:04 +0530

    db update

--- database/db.php
+++ database/db.php
@@ -27,7 +27,7 @@ try {
 login Scheme
 1. user
     CREATE TABLE `messenger`.`user` ( `id` INT NOT NULL , 
-    `username` CHAR(250) NOT NULL , `email` CHAR(250) NOT NULL ,
+    `full_name` CHAR(250) NOT NULL , `email` CHAR(250) NOT NULL ,
     `password` CHAR(250) NOT NULL , `profile_pic` CHAR(250) NOT NULL , 
     `last_login` CHAR(250) NOT NULL , `online` INT(1) NOT NULL ,
     PRIMARY KEY (`id`)) ENGINE = InnoDB;

1ebf104b5d533d6c35f6270bc4d49e3aa96d72e8
rahulshahare <rahulshahare@gmail.com>
2022-04-26 20:49:32 +0530

    CSS & JS for messenger

--- index.php
+++ index.php
@@ -6,13 +6,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="css/bootstrap.min.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/messenger.css" rel="stylesheet">
 
-    <title>Hello, world!</title>
+    <title>Messenger</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <!-- As a heading -->
+   
+    <nav class="navbar navbar-dark bg-primary">
+        <div class="container">
+            <span class="navbar-brand mb-0 h1">Messenger</span>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-md-4 mt-3 p-2 mybox-shadow">
+                <h3 class="text-center">Login</h3>
+            </div>
+        </div>
+    </div>
+
 
     <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/messenger.js"></script>
   </body>
 </html>

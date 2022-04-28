20e829cb9079839b62c76656bc7acc7d4f42588c
rahulshahare <rahulshahare@gmail.com>
2022-04-28 14:55:18 +0530

    no password

--- api/get_user_details.php
+++ api/get_user_details.php
@@ -3,7 +3,7 @@ if(!empty($_GET['userId'])){
     $userId = $_GET['userId'];
     require_once"../database/db.php";
 
-    $stm = $dbh->prepare("SELECT * FROM `user` WHERE id != :id ORDER BY full_name ASC");
+    $stm = $dbh->prepare("SELECT `id`, `full_name`, `email`, `profile_pic`, `register_on`,`last_login`, `online` FROM `user` WHERE id != :id ORDER BY full_name ASC");
     $stm->bindValue(":id", $userId);
     $stm->execute();
     $row = $stm->fetchAll(PDO::FETCH_OBJ);

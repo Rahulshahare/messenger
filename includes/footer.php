46b1dc63ec7784149d41c5c6608c5c09a2f6bb49
rahulshahare <rahulshahare@gmail.com>
2022-04-30 22:55:28 +0530

    PRODUCTIVE UPDATE: 1) comment all console logs

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,8 @@
 <script src="js/bootstrap.bundle.min.js"></script>
 <script src="js/jquery-3.6.0.min.js"></script>
-    <script src="js/messenger.js?v=0.0.6"></script>
+  <?php if($_SERVER['PHP_SELF'] != "/messenger/login.php"  && $_SERVER['PHP_SELF'] != "/messenger/createnewuser.php") {?>
+    <script src="js/messenger.js?v=0.0.7"></script>
     <script src="js/timeago.js"></script>
-    <?php unset($dbh); ?>
+    <?php } unset($dbh); ?>
   </body>
 </html>

1c8b3697a3f05eef04bcd6143475a986a07eab3e
rahulshahare <rahulshahare@gmail.com>
2022-10-10 23:45:57 +0530

    New Minified Version Applied

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <script src="js/bootstrap.bundle.min.js"></script>
 <script src="js/jquery-3.6.0.min.js"></script>
   <?php if($_SERVER['PHP_SELF'] != "/messenger/login.php"  && $_SERVER['PHP_SELF'] != "/messenger/createnewuser.php") {?>
-    <script src="js/messenger.js?v=3"></script>
+    <script src="js/messenger.min.js"></script>
     <script src="js/fancybox.umd.js"></script>
     <script src="js/timeago.js"></script>
     <?php } unset($dbh); ?>
